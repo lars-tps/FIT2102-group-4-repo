928b6814e0ad0b44c57b7bcee54042942ade4398
HardDisco <49183778+lars-tps@users.noreply.github.com>
2022-08-01 10:02:27 +0800

    Add week 2 starter code

---
+++ week 2/main.js
@@ -0,0 +1,53 @@
+// Surname     | Firstname | Contribution % | Any issues?
+// =====================================================
+// Person 1... |           | 25%
+// Person 2... |           | 25%
+// Person 3... |           | 25%
+// Person 4... |           | 25%
+//
+// complete Worksheet 2 by entering code in the places marked below...
+//
+// For full instructions and tests open the file worksheetChecklist.html
+// in Chrome browser.  Keep it open side-by-side with your editor window.
+// You will edit this file (main.js), save it, and reload the
+// browser window to run the test.
+
+/**
+ * Exercise 1
+ */
+
+/**
+ * Exercise 2
+ */
+
+/**
+ * Exercise 3
+ */
+
+/**
+ * Exercise 4
+ */
+
+/**
+ * Exercise 5
+ */
+
+/**
+ * Exercise 6
+ */
+
+/**
+ * Exercise 7
+ */
+
+/**
+ * Exercise 8
+ */
+
+/**
+ * Exercise 9
+ */
+
+/**
+ * Exercise 10
+ */
