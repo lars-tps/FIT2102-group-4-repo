72ec514f121a8a74c5e2867aa418f6b0240fe7c6
oooonji <109935286+oooonji@users.noreply.github.com>
2022-08-08 17:13:52 +0800

    Exercise 1

--- week 3/main.ts
+++ week 3/main.ts
@@ -36,7 +36,7 @@ function numberToString(input: number):string {
  * If 'padding' is a string, then 'padding' is appended to the left side.
  * If 'padding' is a number, then that number of spaces is added to the left side.
  */
-function padLeft(value: string, padding: number | string) {
+function padLeft(value: string, padding: number | string): string {
   if (typeof padding === "number") {
     return Array(padding + 1).join(" ") + value;
   }
@@ -49,7 +49,7 @@ function padLeft(value: string, padding: number | string) {
 padLeft("Hello world", 4); // returns "    Hello world"
 
 // What's the type of arg0 and arg1?
-function curry<U,V,W>(f: (x: U, y: V) => W) {
+function curry<U,V,W>(f: (x: U, y: V) => W): (x: U) => (y: V) => W{
   return function (x: U) {
     return function (y: V) {
       return f(x, y);
