e47620d6e5942dfcefdc9bf5d012ac3f0215bb38
HardDisco <49183778+lars-tps@users.noreply.github.com>
2022-08-15 10:08:18 +0800

    Add them solutions

--- week 3/main.ts
+++ week 3/main.ts
@@ -23,7 +23,6 @@ const IMPLEMENT_THIS: any = undefined;
 /*****************************************************************
  * Exercise 1
  */
-
 function addStuff(a:number, b:number):number {
   return a + b;
 }
@@ -116,14 +115,79 @@ function forEach<T>(f: (_: T) => void, list: ConsList<T>): void {
 
 /**
  * Implement this function! Also, complete this documentation (see forEach).
+ * @param f Function to be called on each element
+ * @param list Cons list
  */
 function map<T, V>(f: (_: T) => V, l: ConsList<T>): ConsList<V> {
-  return IMPLEMENT_THIS;
+  if (l) {
+    return cons(f(head(l)), map(f, rest(l)))
+  } else {
+    return null
+  }
 }
-
 /*****************************************************************
  * Exercise 3
  */
+
+/**
+ * Turns an array into a ConsList
+ * @param arr an array
+ * @returns a cons list
+ */
+function fromArray<T>(arr:T[]): ConsList<T> {
+  return arr.length > 0 ? cons(arr[0], fromArray(arr.slice(1))) : null
+}
+
+/**
+ * Reduce all data that are in a ConsList into a single accmulated valued, starts accmulation at the start of ConsList
+ * @param func a function to reduce by
+ * @param accumulator the initial value to start accumulating
+ * @param list a ConsList
+ * @returns a single value
+ */
+function reduce<T,V>(func: (x:V, y:T) => V, accumulator: V, list: ConsList<T>): V {
+  return list ? reduce(func, func(accumulator, head(list)), rest(list)) : accumulator
+}
+
+/**
+ * Reduce all data that are in a ConsList into a single accmulated valued, starts accumulation from the end of ConsList
+ * @param func a function to reduce by
+ * @param accumulator the initial value to start accumulating
+ * @param list a ConsList
+ * @returns a single value
+ */
+function reduceRight<T,V>(func: (x:V, y:T) => V, accumulator: V, list: ConsList<T>): V {
+  return list ? func(reduceRight(func, accumulator, rest(list)), head(list)) : accumulator
+}
+
+/**
+ * Filters all data in provided ConsList based on a truth function
+ * @param func truth function that evaluates to a boolean value
+ * @param list a ConsList
+ * @returns a new and filtered ConsList
+ */
+function filter<T>(func: (head:T) => boolean, list: ConsList<T>): ConsList<T> {
+  return list ? func(head(list)) ? cons(head(list), filter(func, rest(list))) : filter(func, rest(list)) : null
+}
+
+/**
+ * Concatenates two ConsList together into a new ConsList
+ * @param list1 a ConsList
+ * @param list2 another ConsList
+ * @returns a new ConsList with data from list1 and list2 in that order
+ */
+function concat<T>(list1: ConsList<T>, list2: ConsList<T>): ConsList<T> {
+  return list1 ? cons(head(list1), concat(rest(list1),list2)) : list2 ? cons(head(list2), concat(list1, rest(list2))) : null
+}
+
+/**
+ * Reverses a ConsList (it just works)
+ * @param list a ConsList
+ * @returns a reversed ConsList
+ */
+function reverse<T>(list: ConsList<T>) : ConsList<T> {
+  return list ? rest(list) ? cons(head(reverse(rest(list))), reverse(rest(rest(list)))): cons(head(list) , null) : null
+}
 
 // Example use of reduce
 function countLetters(stringArray: string[]): number {
@@ -146,7 +210,7 @@ class List<T> {
 
   constructor(list: T[] | ConsList<T>) {
     if (list instanceof Array) {
-      // IMPLEMENT THIS. What goes here ??
+      this.head = fromArray(list)
     } else {
       // nullish coalescing operator
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
@@ -165,11 +229,40 @@ class List<T> {
   }
 
   // Add methods here:
+  forEach(f: (_:T) => void): List<T>{
+    forEach(f, this.head)
+    // return this
+    return new List(this.head)
+  }
+
+  filter(f: (_:T) => boolean): List<T> {
+    return new List(filter(f, this.head))
+  }
+
+  reduce<V>(f: (accumulator:V, t:T) => V, initialValue: V): V {
+    return reduce(f, initialValue, this.head)
+  }
+
+  concat(other: List<T>): List<T> {
+    return new List(concat(this.head, other.head))
+  }
+
+  map<V>(f: (_:T) => V): List<V> {
+    return new List(map(f, this.head))
+  }
 }
 
 /*****************************************************************
  * Exercise 5
  */
+
+function line(text: string): [number, string] {
+  return [0, text]
+}
+
+function lineToList(line: [number, string]): List<[number, string]> {
+  return new List([line])
+}
 
 /*****************************************************************
  * Exercise 6
@@ -192,22 +285,26 @@ const myTree = new BinaryTreeNode(
   new BinaryTreeNode(4)
 );
 
+function nest (indent: number, layout: List<[number, string]>): List<[number, string]> {
+  return layout.map((line: [number, string]) => [line[0]+indent, line[1]])
+}
+
 // *** uncomment the following code once you have implemented List and nest function (above) ***
 
-// function prettyPrintBinaryTree<T>(node: BinaryTree<T>): List<[number, string]> {
-//     if (!node) {
-//         return new List<[number, string]>([])
-//     }
-//     const thisLine = lineToList(line(node.data.toString())),
-//           leftLines = prettyPrintBinaryTree(node.leftChild),
-//           rightLines = prettyPrintBinaryTree(node.rightChild);
-//     return thisLine.concat(nest(1, leftLines.concat(rightLines)))
-// }
+function prettyPrintBinaryTree<T>(node: BinaryTree<T>): List<[number, string]> {
+    if (!node) {
+        return new List<[number, string]>([])
+    }
+    const thisLine = lineToList(line(node.data.toString())),
+          leftLines = prettyPrintBinaryTree(node.leftChild),
+          rightLines = prettyPrintBinaryTree(node.rightChild);
+    return thisLine.concat(nest(1, leftLines.concat(rightLines)))
+}
 
-// const output = prettyPrintBinaryTree(myTree)
-//                     .map(aLine => new Array(aLine[0] + 1).join('-') + aLine[1])
-//                     .reduce((a,b) => a + '\n' + b, '').trim();
-// console.log(output);
+const output = prettyPrintBinaryTree(myTree)
+                    .map(aLine => new Array(aLine[0] + 1).join('-') + aLine[1])
+                    .reduce((a,b) => a + '\n' + b, '').trim();
+console.log(output);
 
 /*****************************************************************
  * Exercise 7: Implement prettyPrintNaryTree, which takes a NaryTree as input
@@ -233,7 +330,13 @@ const naryTree = new NaryTree(
 
 // Implement: function prettyPrintNaryTree(...)
 function prettyPrintNaryTree<T>(node: NaryTree<T>): List<[number, string]> {
-  return IMPLEMENT_THIS;
+  // if (!node) {
+  //   return new List<[number, string]>([])
+  // }
+  // const thisLine = lineToList(line(node.data.toString()))
+  // return thisLine.concat(nest(1, node.children.map(prettyPrintNaryTree).reduce(concat, thisLine))
+
+  node ? node.children ? nest(1, prettyPrintNaryTree(node.children)) : lineToList(line(node.data.toString())) : new List<[number, string]>([])
 }
 
 // *** uncomment the following code once you have implemented prettyPrintNaryTree (above) ***
